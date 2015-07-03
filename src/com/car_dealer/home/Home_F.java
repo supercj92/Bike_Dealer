4ce4acd6454c84d8ac9d98bf522f4c861411acf7
IData <ukd163@163.com>
2015-07-03 17:36:09 +0800

    7_3

---
+++ src/com/car_dealer/home/Home_F.java
@@ -0,0 +1,100 @@
+package com.car_dealer.home;
+
+
+import java.util.ArrayList;
+
+import com.example.car_dealer.R;
+import com.javis.Adapter.Adapter_GridView;
+import com.javis.ab.view.AbOnItemClickListener;
+import com.javis.ab.view.AbSlidingPlayView;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.AdapterView.OnItemClickListener;
+
+public class Home_F extends Fragment {
+	
+	// 分类九宫格的资源文件
+	private int[] pic_path_classify = { R.drawable.menu_guide_2, R.drawable.menu_guide_3, R.drawable.menu_guide_4, R.drawable.menu_guide_5, R.drawable.menu_guide_6, R.drawable.menu_guide_7, R.drawable.menu_guide_8 };
+	//分类的九宫格
+	private GridView gridView_classify;
+	//gridview适配器
+	private Adapter_GridView adapter_GridView_classify;
+	public AbSlidingPlayView viewPager;
+	/**存储首页轮播的界面*/
+	private ArrayList<View> allListView;
+	/**首页轮播的界面的资源*/
+	private int[] resId = { R.drawable.show_m1, R.drawable.menu_viewpager_1, R.drawable.menu_viewpager_2, R.drawable.menu_viewpager_3, R.drawable.menu_viewpager_4, R.drawable.menu_viewpager_5 };
+
+	@Override
+	public View onCreateView(LayoutInflater inflater,
+			@Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+		View view = LayoutInflater.from(getActivity()).inflate(R.layout.home, null);
+		initView(view);
+		return view;
+	}
+	
+	private void initView(View view){
+		
+		gridView_classify = (GridView) view.findViewById(R.id.my_gridview);
+		gridView_classify.setSelector(new ColorDrawable(Color.TRANSPARENT));
+		adapter_GridView_classify = new Adapter_GridView(getActivity(), pic_path_classify);
+		gridView_classify.setAdapter(adapter_GridView_classify);
+		
+		gridView_classify.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+				//挑战到宝贝搜索界面
+				//Intent intent = new Intent(getActivity(), WareActivity.class);
+				//startActivity(intent);
+			}
+		});
+		
+		viewPager = (AbSlidingPlayView) view.findViewById(R.id.viewPager_menu);
+		//设置播放方式为顺序播放
+		viewPager.setPlayType(1);
+		//设置播放间隔时间
+		viewPager.setSleepTime(3000);
+		initViewPager();
+	}
+	
+	private void initViewPager() {
+
+		if (allListView != null) {
+			allListView.clear();
+			allListView = null;
+		}
+		allListView = new ArrayList<View>();
+
+		for (int i = 0; i < resId.length; i++) {
+			//导入ViewPager的布局
+			View view = LayoutInflater.from(getActivity()).inflate(R.layout.pic_item, null);
+			ImageView imageView = (ImageView) view.findViewById(R.id.pic_item);
+			imageView.setImageResource(resId[i]);
+			allListView.add(view);
+		}
+		
+		
+		viewPager.addViews(allListView);
+		//开始轮播
+		viewPager.startPlay();
+		viewPager.setOnItemClickListener(new AbOnItemClickListener() {
+			@Override
+			public void onClick(int position) {
+				//跳转到详情界面
+			/*	Intent intent = new Intent(getActivity(), BabyActivity.class);
+				startActivity(intent);*/
+			}
+		});
+	}
+}
---
+++ src/com/car_dealer/home/Main_FA.java
@@ -0,0 +1,110 @@
+package com.car_dealer.home;
+
+
+import com.example.car_dealer.R;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+
+public class Main_FA extends FragmentActivity implements OnClickListener {
+
+	// 界面底部的菜单按钮
+		private ImageView[] bt_menu = new ImageView[4];
+		// 界面底部的菜单按钮id
+		private int[] bt_menu_id = { R.id.iv_menu_0, R.id.iv_menu_1, R.id.iv_menu_2, R.id.iv_menu_4 };
+
+		// 界面底部的选中菜单按钮资源
+		private int[] select_on = { R.drawable.guide_home_on, R.drawable.guide_tfaccount_on, R.drawable.guide_discover_on, R.drawable.guide_account_on };
+		// 界面底部的未选中菜单按钮资源
+		private int[] select_off = { R.drawable.bt_menu_0_select, R.drawable.bt_menu_1_select, R.drawable.bt_menu_2_select,  R.drawable.bt_menu_4_select };
+
+		/** 主界面 */
+		private Home_F home_F;
+		
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		initView();
+	}
+	
+	private void initView(){
+		for (int i = 0; i < bt_menu.length; i++) {
+			bt_menu[i] = (ImageView) findViewById(bt_menu_id[i]);
+			bt_menu[i].setOnClickListener(this);
+		}
+		
+		// 初始化默认显示的界面
+				if (home_F == null) {
+					home_F = new Home_F();
+					addFragment(home_F);
+					showFragment(home_F);
+				} else {
+					showFragment(home_F);
+				}
+		
+		// 设置默认首页为点击时的图片
+		bt_menu[0].setImageResource(select_on[0]);
+	}
+	
+	@Override
+	public void onClick(View v) {
+	
+		switch (v.getId()) {
+		case R.id.iv_menu_0:
+			// 主界面
+			if (home_F == null) {
+				home_F = new Home_F();
+				// 判断当前界面是否隐藏，如果隐藏就进行添加显示，false表示显示，true表示当前界面隐藏
+				addFragment(home_F);
+				showFragment(home_F);
+			} else {
+				if (home_F.isHidden()) {
+					showFragment(home_F);
+				}
+			}
+
+			break;
+	
+		}
+		
+		// 设置按钮的选中和未选中资源
+		for (int i = 0; i < bt_menu.length; i++) {
+			bt_menu[i].setImageResource(select_off[i]);
+			if (v.getId() == bt_menu_id[i]) {
+				bt_menu[i].setImageResource(select_on[i]);
+			}
+		}
+	}
+	
+	/** 添加Fragment **/
+	public void addFragment(Fragment fragment) {
+		FragmentTransaction ft = this.getSupportFragmentManager().beginTransaction();
+		ft.add(R.id.show_layout, fragment);
+		ft.commit();
+	}
+	
+	/** 显示Fragment **/
+	public void showFragment(Fragment fragment) {
+		FragmentTransaction ft = this.getSupportFragmentManager().beginTransaction();
+		// 设置Fragment的切换动画
+		ft.setCustomAnimations(R.anim.cu_push_right_in, R.anim.cu_push_left_out);
+
+		// 判断页面是否已经创建，如果已经创建，那么就隐藏掉
+		if (home_F != null) {
+			ft.hide(home_F);
+		}
+		ft.show(fragment);
+		ft.commitAllowingStateLoss();
+
+	}
+	
+}
